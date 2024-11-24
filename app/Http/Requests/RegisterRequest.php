91d18d6ba4c355afc80fd4f3b479d92b5940ab52
Cmilja <dsmiljanic40@gmail.com>
2024-11-24 18:32:51 +0100

    Register request added

--- app/Http/Requests/RegisterRequest.php
+++ app/Http/Requests/RegisterRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class LoginRequest extends FormRequest
+class RegisterRequest extends FormRequest
 {
     public function authorize(): bool
     {
@@ -14,8 +14,11 @@ class LoginRequest extends FormRequest
     public function rules(): array
     {
         return [
+            'first_name' => 'required|string|max:255',
+            'last_name' => 'required|string|max:255',
             'email' => 'required|email|max:255',
-            'password' => 'required|min:8'
+            'phone' => 'required|string|max:15',
+            'password' => 'required|min:8|confirmed'
         ];
     }
 }
