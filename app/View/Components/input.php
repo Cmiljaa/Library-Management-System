47c361462e9372a3c8cb47ba1b4726e5e6d010f7
Cmilja <dsmiljanic40@gmail.com>
2024-11-24 03:15:46 +0100

    Added login and register routes

--- app/View/Components/input.php
+++ app/View/Components/input.php
@@ -16,6 +16,7 @@ class input extends Component
         public string $name = '',
         public string $id = '',
         public string $value = '',
+        public string $placeholder = '',
         public bool $required = false
     )
     {
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,17 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', function(){
+    return redirect(route('auth.login'));
+});
+
+Route::prefix('/auth')->group(function(){
+
+    Route::get('/login', function(){
+        return view('login');
+    })->name('auth.login');
+    
+    Route::get('/register', function(){
+        return view('register');
+    })->name('auth.register');
 });
