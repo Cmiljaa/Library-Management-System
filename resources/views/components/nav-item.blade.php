76acdfe6c74f664da6e967a0b67046594e18fcd2
Cmilja <dsmiljanic40@gmail.com>
2024-11-28 21:10:25 +0100

    Added navitem component

---
+++ resources/views/components/nav-item.blade.php
@@ -0,0 +1,6 @@
+<li class="pb-3 pt-2">
+    <a href="{{ $href }}" 
+    class="block text-2xl text-white font-medium lg:hover:bg-transparent hover:text-[#ffccbc] focus:outline-none focus:ring-2 hover:bg-[#5d4037] focus:ring-[#8d6e63] px-3 py-2 rounded-md">
+        {{ $slot }}
+    </a>
+</li>
