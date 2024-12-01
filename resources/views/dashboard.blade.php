efe4b26e6cec4d4d77de1fda54d9b96fd41eb649
Cmilja <dsmiljanic40@gmail.com>
2024-12-01 19:54:46 +0100

    Showing all books

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -17,9 +17,55 @@
             <x-select name="language" :array="config('book.languages')" />
         </div>
         <div class="w-full max-w-md md:w-auto">
-            <x-button class="bg-blue-100">
+            <x-button>
                 Submit
             </x-button>
         </div>
     </div>
+</div>
+
+<div class="container mx-auto p-4 space-y-6">
+    @forelse ($books as $book)
+        <div class="bg-white shadow-md rounded-lg p-4 w-full max-w-2xl mx-auto">
+            <h2 class="text-2xl font-bold text-gray-900 mb-2 tracking-wide">
+                {{ $book->title }}
+            </h2>
+
+            <div class="flex justify-between items-center mb-4 mt-5">
+                <div>
+                    <p class="text-md text-gray-600 mb-4">
+                        <strong class="text-gray-800">Author:</strong> {{ $book->author }}
+                    </p>
+                </div>
+
+                <div class="flex space-x-2">
+                    <button class="rounded-sm text-black hover:bg-black hover:text-white px-3 py-1 border border-black transition-colors duration-300 ease-in-out text-sm">
+                        {{ Str::title(str_replace('_', ' ', $book->genre)) }}
+                    </button>
+                    
+                    <button class="rounded-sm text-black hover:bg-black hover:text-white px-3 py-1 border border-black transition-colors duration-300 ease-in-out text-sm">
+                        {{ Str::title( $book->language) }}
+                    </button>                                 
+                </div>
+            </div>
+
+            <div class="mt-4">
+                <a href="/books/{{ $book->id }}" class="inline-block">
+                    <x-button>
+                        Show
+                    </x-button>
+                </a>
+            </div>
+        </div>
+    @empty
+        <div class="text-center text-black text-xl">
+            No books available at the moment.
+        </div>
+    @endforelse
+
+    @if ($books->count())
+        <div class="flex justify-center mt-8">
+            {{ $books->links('pagination::tailwind') }}
+        </div>
+    @endif
 </div>
