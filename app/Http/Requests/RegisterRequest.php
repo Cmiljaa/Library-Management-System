399bf666e7e8d84eba1b1a3a960ffef32103aacc
Cmilja <dsmiljanic40@gmail.com>
2024-12-08 02:24:38 +0100

    Register update request fix

--- app/Http/Requests/RegisterRequest.php
+++ app/Http/Requests/RegisterRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Validation\Rule;
 
 class RegisterRequest extends FormRequest
@@ -14,12 +15,19 @@ class RegisterRequest extends FormRequest
 
     public function rules(): array
     {
-        return [
+        $rules = [
             'first_name' => 'required|string|max:255',
             'last_name' => 'required|string|max:255',
-            'email' => 'required|email|max:255|unique:users,email', Rule::unique('users', 'email')->ignore(optional($this->user)->id),
+            'email' => 'required|email|max:255|unique:users,email,',
             'phone' => 'required|string|max:15',
             'password' => 'required|min:8|confirmed'
         ];
+
+        if($this->getMethod() === 'PUT')
+        {
+            $rules['email'] = $rules['email']  . Auth::user()->id;
+        }
+
+        return $rules;
     }
 }
