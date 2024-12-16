a1539af938ec64d8798999415f926ac540797681
Cmilja <dsmiljanic40@gmail.com>
2024-12-16 22:55:52 +0100

    Book service dependency injection

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\BookRequest;
 use App\Models\Book;
 use App\Services\BookService;
 use App\Services\ReviewService;
@@ -9,9 +10,16 @@ use Illuminate\Http\Request;
 
 class BookController extends Controller
 {
+    protected $bookService;
+
+    public function __construct(BookService $bookService)
+    {
+        $this->bookService = $bookService;
+    }
+
     public function index(Request $request)
     {
-        return view('books.index', ['books' => BookService::getAllBooks($request)]);
+        return view('books.index', ['books' => $this->bookService->getAllBooks($request)]);
     }
 
     public function create()
--- app/Services/BookService.php
+++ app/Services/BookService.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 
 class BookService
 {
-    public static function getAllBooks(Request $request)
+    public function getAllBooks(Request $request)
     {
         return Book::query()->withAvg('reviews', 'rating')->FilterBySearch($request)->FilterByAttribute($request, ['genre', 'language'])->latest()->paginate(10);
     }
