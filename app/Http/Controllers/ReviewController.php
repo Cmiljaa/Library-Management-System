ba16a728ddeaa76b722dfae3b584cc9e970321fd
Cmilja <dsmiljanic40@gmail.com>
2024-12-20 23:45:14 +0100

    Review create logic

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -2,18 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\ReviewRequest;
+use App\Services\ReviewService;
 use Illuminate\Http\Request;
 
 class ReviewController extends Controller
 {
+    protected $reviewService;
+
+    public function __construct(ReviewService $reviewService)
+    {
+        $this->reviewService = $reviewService;        
+    }
+
     public function index()
     {
         //
     }
 
-    public function store(Request $request)
+    public function store(ReviewRequest $request)
     {
-        //
+        $this->reviewService->createReview($request->validated());
+        return redirect()->back()->with('success', 'Review added successfully');
     }
     
     public function update(Request $request, string $id)
--- app/Services/ReviewService.php
+++ app/Services/ReviewService.php
@@ -4,11 +4,25 @@ namespace App\Services;
 
 use App\Models\Book;
 use App\Models\Review;
+use Illuminate\Support\Facades\Auth;
 
 class ReviewService
 {
-    public static function getBookReviews(Book $book)
+    public function getBookReviews(Book $book)
     {
-        return Review::where('book_id', $book->id)->with('user')->get();
+        return Review::where('book_id', $book->id)->with('user')->latest()->get();
+    }
+
+    public function createReview(array $credentials): void
+    {
+        try
+        {
+            $credentials['user_id'] = Auth::id();
+            Review::create($credentials);
+        }
+        catch (\Exception $th)
+        {
+            abort(500, 'An error occurred while creating your review.');
+        }
     }
 }
