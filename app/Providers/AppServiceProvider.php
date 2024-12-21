ca203fefa0aec210dbefd0f5ea7265c6e01f0fe1
Cmilja <dsmiljanic40@gmail.com>
2024-12-22 00:52:50 +0100

    Added canAccessReview and isUserSelf gate

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,8 +20,16 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Gate::define('allowed', function ($authUser, $userBeingAuthorized) {
-            return $authUser->role === 'admin' || $authUser->role === 'librarian' || $authUser->id === $userBeingAuthorized->id;
+        Gate::define('isUserSelf', function($authUser, $userBeingAuthorized){
+            return $authUser->id === $userBeingAuthorized->id;
+        });
+
+        Gate::define('canAccessReview', function($authUser, $review){
+            return $authUser->id === $review->user_id;
+        });
+        
+        Gate::define('isUserSelfOrAdminLibrarian', function ($authUser, $userBeingAuthorized) {
+            return $authUser->role === 'admin' || $authUser->role === 'librarian' || Gate::allows('isUserSelf', $userBeingAuthorized);
         });
     }
 }
