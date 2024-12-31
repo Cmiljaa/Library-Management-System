613d6c46e73712f507385c54f9d23ddace791647
Cmilja <dsmiljanic40@gmail.com>
2024-12-31 19:51:50 +0100

    Fixed links in footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -5,10 +5,10 @@
                 <h5 class="uppercase font-semibold mb-4">Resources</h5>
                 <ul class="space-y-2">
                     <li class="hover:text-gray-600">
-                        <a href="#">Laravel 11</a>
+                        <a href="https://laravel.com" target="_blank">Laravel 11</a>
                     </li>
                     <li class="hover:text-gray-600">
-                        <a href="#">Bootstrap 5.3.3</a>
+                        <a href="https://tailwindcss.com" target="_blank">Tailwind CSS 3</a>
                     </li>
                 </ul>
             </div>
@@ -17,10 +17,10 @@
                 <h5 class="uppercase font-semibold mb-4">Legal</h5>
                 <ul class="space-y-2">
                     <li class="hover:text-gray-600">
-                        <a href="#">Privacy Policy</a>
+                        <a href="{{ route('legal.privacy_policy') }}">Privacy Policy</a>
                     </li>
                     <li class="hover:text-gray-600">
-                        <a href="#">Terms & Conditions</a>
+                        <a href="{{ route('legal.terms_and_conditions') }}">Terms & Conditions</a>
                     </li>
                 </ul>
             </div>
@@ -29,9 +29,9 @@
         <div class="border-t border-gray-700 my-6"></div>
 
         <div class="flex flex-col md:flex-row justify-between items-center">
-            <span class="text-center">© 2024 All Rights Reserved by Cmiljaaa</span>
+            <span class="text-center">© 2025 All Rights Reserved by Cmiljaaa</span>
             <div class="flex space-x-4 mt-4 md:mt-0">
-                <a href="#" class="text-black hover:text-gray-600">
+                <a href="https://github.com/Cmiljaa/Library-Management-System" target="_blank" class="text-black hover:text-gray-600">
                     <i class="fab fa-github fa-2x"></i>
                 </a>
             </div>
