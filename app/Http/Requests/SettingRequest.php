30c7ea51f622bb7214262991e36544861c6fda96
Cmilja <dsmiljanic40@gmail.com>
2025-01-04 18:19:40 +0100

    Setting request

---
+++ app/Http/Requests/SettingRequest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class SettingRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'value' => $this->getValueValidationRule(),
+        ];
+    }
+
+    protected function getValueValidationRule()
+    {
+        switch ($this->route('setting')->type)
+        {
+            case 'integer':
+                return 'required|integer|min:1';
+
+            case 'string':
+                return 'required|string|min:1|max:255';
+            
+            default:
+                return 'required';
+        }
+    }
+}
