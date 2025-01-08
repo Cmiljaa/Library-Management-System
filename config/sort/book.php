d4a2e4db6fe9c5eb810f4adaaee0f992e941d357
Cmilja <dsmiljanic40@gmail.com>
2025-01-08 23:51:56 +0100

    Added book sorting file and fixed book_loan sorting file

---
+++ config/sort/book.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'book_loans_count, desc' => 'Most Popular',
+    'book_loans_count, asc' => 'Least Popular',
+    'reviews_count, desc' => 'Reviews (Higher to Lower)',
+    'reviews_count, asc' => 'Reviews (Lower to Higher)',
+    'created_at, desc' => 'Newest to Oldest',
+    'created_at, asc' => 'Oldest to Newest',
+];
--- config/sort/book_loan.php
+++ config/sort/book_loan.php
@@ -1,12 +1,12 @@
 <?php
 
 return [
-    'created_at, asc' => 'Oldest to Newest',
     'created_at, desc' => 'Newest to Oldest',
-    'borrow_date, asc' => 'Borrow Date (Oldest to Newest)',
+    'created_at, asc' => 'Oldest to Newest',
     'borrow_date, desc' => 'Borrow Date (Newest to Oldest)',
-    'return_date, asc' => 'Return Date (Oldest to Newest)',
+    'borrow_date, asc' => 'Borrow Date (Oldest to Newest)',
     'return_date, desc' => 'Return Date (Newest to Oldest)',
+    'return_date, asc' => 'Return Date (Oldest to Newest)',
     'status, asc' => 'Status (A to Z)',
     'status, desc' => 'Status (Z to A)',
 ];
